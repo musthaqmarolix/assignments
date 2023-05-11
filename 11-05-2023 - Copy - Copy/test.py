98fff38d494c9afa44da7854b65db1dec9aceb5a
musthaq <musthaqahmed.marolix@gmail.com>
2023-05-11 11:55:21 +0530

    intial commit

---
+++ 11-05-2023 - Copy - Copy/test.py
@@ -0,0 +1,59 @@
+# s="mohan is good boy"
+# print(s[1:5:2])
+# print(len())
+#list
+# l=[10,20,30,40,'aman']
+# print(l.append(None))
+
+#tuple
+
+# t=(10,20,[5
+# 0,30],30)
+# print(t[2])
+
+#range
+
+# range(10,20) 
+
+# s={10,20,30}
+# print(s.remove(20))
+# print(s)
+
+#dict
+
+# d={1:"abc",2:"ced"}
+# print(d[1])
+
+# if x:
+#     action-1
+# elif:
+#     action-2
+# elif:
+# action-3
+# else:
+# action-4
+
+# n1=int(input('enter the number'))
+# n2=int(input('enter the number'))
+# if n1>n2:
+#     print("the bigger number is ", n1)
+# else:
+#     print("the bigger number is ",n2)
+
+# #selection statements
+# name=input("enter your name")
+
+n=int(input("enter the number")) 
+# if n>=1 and n<=100:
+#     print("the number",n, " is in between 1 and 100")
+# else:
+#     print("the number" , n , "is not in b/w 1 and 100")
+     
+
+if n==0:
+    print("zero")
+elif n==1:
+    print("one")
+elif n==2:
+    print("two")
+else:print("enter number from 0 to 1 only")
